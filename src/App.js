2dcf0bb36dbdda51b0d5d34d43b6a3068b4a3d37
chensiyuan <heartofblack@163.com>
2018-12-25 22:33:26 +0800

    理解修饰器

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
 import React, {Component} from 'react';
 
-import {observable} from 'mobx';
+import {observable, action, computed} from 'mobx';
 import {observer, Provider} from 'mobx-react'
 import Test from './model/test';
 import './App.css';
 import './index.css';
+
+function mylog(target) {
+
+    const pro = Object.getOwnPropertyDescriptors(target.prototype);
+    for (const key in pro) {
+
+        if (key === 'constructor') {
+            continue;
+        }
+
+        const func = pro[ key ].value;
+        if (typeof func === 'function') {
+            Object.defineProperty(target.prototype, key, {
+                value(...args) {
+                    console.log('重新定义的function');
+                     for (let val of args) {
+                        if (typeof val !== 'number') {
+                            throw Error(`${val} is not number`)
+
+                        }
+                       
+                    } 
+                    return func.apply(this, args);
+                }
+
+            })
+
+
+        }
+    }
+}
+
 @observer
+@mylog
 class App extends React.Component {
     constructor() {
         super();
-       this.test=new Test();
+        this.test = new Test();
     }
+    // @action
+    setName (...args) {
+        // this.test.name = 'newName'
+        this.test.state.state1[0] = 666;
+        console.log('设置名称的动作');
+    }
+    @computed get getname() {
 
-    setName = () => {
-        this.test.name = 'newName'
+        return this.test.name;
+
     }
 
     render() {
         return (
             <div>
-                <div className="name">{this.test.name}</div>
+                <div className="name">{this.getname}</div>
+                <div className="name">{this.test.state.state1}</div>
+                <div className="name">{this.test.state.state2}</div>
 
-                <button className="changename" onClick={this.setName}>
+                <button className="changename" onClick={()=>{this.setName(1,2,3,6,'x')}}>
                     change name
                 </button>
             </div>
--- src/model/test.js
+++ src/model/test.js
@@ -1,20 +1,31 @@
 import {observer} from 'mobx-react'
 import { observable, reaction } from 'mobx';
-class TestModal{
-@observable name='myname';
-@observable age=25;
-@observable hight=175;
-setnameReaction=reaction(()=>this.name,name=>{
-
-console.log('channg name reaction and new name is :',name);
-
-})
 
 
 
+class TestModal {
+     hehe='hgehe'
+    @observable name = 'myname';
+    @observable age = 25;
+    @observable hight = 175;
+    @observable state = {
+        state1: [11,2],
+        state2: 2
 
+  }
+  
+    setnameReaction = reaction(() => this.name, name => {
 
+        console.log('channg name reaction and new name is :', name);
 
+    })
+  
+  
+  stateReaction = reaction(()=>this.state.state1.map(item=>item), (state)=> {
+    
+    console.log('state状态发生改变',state);
+
+  })
 
 }
 export default TestModal;
